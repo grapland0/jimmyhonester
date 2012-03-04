cd43d208b39d5c2aee4ee9826671d2aa5759757c
grapland@gmail.com <grapland@gmail.com@883ad577-e22f-63e5-9870-344c597a37cf>
2012-03-04 12:54:40 +0000

    Add multi-problem support

--- trunk/src/check.py
+++ trunk/src/check.py
@@ -19,6 +19,8 @@ def main(fname):
 	rd=rrreader(fname)
 	print "Reading running records",
 	mtd={}
+	pids={}
+	uids={}
 	cnt=0
 	for it in rd:
 		print "%d"%cnt,
@@ -37,31 +39,61 @@ def main(fname):
 			print "\nCannot determine the MTV of code: %s."%it.rid
 		else:
 			mtd[it.rid]=mtv
+			if(it.pid not in pids):
+				pids[it.pid]=[]
+			pids[it.pid].append(it.rid)
+			uids[it.rid]=it.uid
+
+
+
 	print "\nMatching."
-	kys=mtd.keys()
-	serf={}
-	allmtvs=[]
-	print "Calc self similarity."
-	for ki in kys:
-		serf[ki]=calcmatch(mtd[ki],mtd[ki])
-	print "Calc mutual similarity."
+	#stat total work load
+	cnt=0.0
 	cntn=0.0
-	for i in xrange(0,len(kys)):
-		mtvs=[]
-		for j in xrange(0,i):
-			cntn+=1.0
-			ki,kj=(kys[i],kys[j])
-			res=calcmatch(mtd[ki],mtd[kj])
-			mtvs.append((kj,res,))
-			allmtvs.append((ki,kj,math.fabs(res/serf[ki]-1.0)))
-		mtvs.sort(key=lambda x:math.fabs(x[1]-serf[ki]))
-		print("[%.1f%%] Top 10 similar with Code %s (MTV:%.4lf)"%(cntn*100.0/float(len(kys)**2/2),ki,serf[ki]))
-		for j in xrange(0,min(10,len(mtvs))):
-			print "\t%s: %.4lf"%(mtvs[j][0],mtvs[j][1])
-	allmtvs.sort(key=lambda x:x[2])
-	print "\nIn all code,Top 100 similar pairs are:"
-	for i in xrange(0,min(100,len(allmtvs))):
-		print "%s with %s, diff:%.5lf"%(allmtvs[i][0],allmtvs[i][1],allmtvs[i][2])
+	for it in pids.itervalues():
+		n=float(len(it))
+		cnt+=n*(n-1.0)/2.0
+	#iter problems
+	for pid,kys in pids.iteritems():
+		print("Working on problem %d."%pid)
+		serf={}
+		allmtvs=[]
+		print "\tCalc self similarity."
+		for ki in kys:
+			serf[ki]=calcmatch(mtd[ki],mtd[ki])
+		print "\tCalc mutual similarity."
+		ress=[[9.9 for x in xrange(len(kys))] for y in xrange(len(kys))]
+
+		#calc similarity matrix
+		for i in xrange(len(kys)):
+			cntn+=1
+			print "%.1lf%%"%(100.0*cntn/cnt),
+			sys.stdout.flush()
+			for j in xrange(i):
+				cntn+=1.0
+				ki,kj=(kys[i],kys[j])
+				if(uids[ki]==uids[kj]):
+					continue
+				ress[i][j]=ress[j][i]=calcmatch(mtd[ki],mtd[kj])
+
+		#display results
+		for i in xrange(len(kys)):
+			mtvs=[]
+			for j in xrange(len(kys)):
+				ki,kj=(kys[i],kys[j])
+				if(uids[ki]==uids[kj]):
+					continue
+				res=ress[i][j]
+				mtvs.append((kj,res,math.fabs(res/serf[ki]-1.0)))
+				if(i<j):allmtvs.append((ki,kj,math.fabs(res/serf[ki]-1.0)))
+			mtvs.sort(key=lambda x:math.fabs(x[1]-serf[ki]))
+			print("\tTop 10 similar with Code %s (MTV:%.4lf)"%(ki,serf[ki]))
+			for j in xrange(0,min(10,len(mtvs))):
+				print "\t\t%s: %.4lf(%.5lf)"%(mtvs[j][0],mtvs[j][1],mtvs[j][2])
+		allmtvs.sort(key=lambda x:x[2])
+		print "\n\tIn all code,Top 100 similar pairs are:"
+		for i in xrange(0,min(100,len(allmtvs))):
+			print "\t\t%s with %s, diff:%.5lf"%(allmtvs[i][0],allmtvs[i][1],allmtvs[i][2])
 
 
 if __name__=='__main__':
--- trunk/src/pkbuild.py
+++ trunk/src/pkbuild.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python
 import os,sys,sqlite3
 
-"""this app recevice a sql bulk export from your OJ and a data file,
+"""
+	this app recevice a sql bulk export from your OJ and svevral data files.
 	output a sqlite db which contain these info.
+	Usage: prog <sql input filename> <data 1 pid> <data 1 filename> ... <data n pid> <data n filename> <output sqlite db>
+
 	the sql bulk info must be all this:
-	insert into codes (cid,code,lid) values (zzzzzz)
+	"insert into codes (cid,code,lid,pid,uid) values (zzzzzz)"
+	cid:	code id,in INT, this id will be used to identify the code in output.
+	code:	source code,in TEXT,quoted by SQL standard.
+	lid:	language id, in INT, spec the language CODEs are. 0 for C, 1 for CPP, 2 for Java.
+	pid:	problem id, in INT, spec the problem, used for selecting data and partition task.
+	uname:	user name,in int,spec the user, used to avoid judge between codes written by same user.
+
+	In data section, the pid must match the codes' you use in sql bulk.
 """
 
 #when use multi_line,this func return a sql stmt when given a file reader iter.
 def getnxtsql(ifi):
-	a=ifi.next().strip()
-	return a+ifi.next().strip()
+	sql=''
+	while(True):
+		sql+=ifi.next()
+		if(sqlite3.complete_statement(sql)):
+			if(sql.lstrip().upper().startswith("INSERT")):
+				return sql
+			else:
+				raise Exception("The sql bulk file contains not only INSERTs.")
 
 def main(fsql,fdata,opt):
 	db=sqlite3.connect(opt)
 	c=db.cursor()
 	try:
 		c.execute("drop table codes")
-		c.execute("drop table data")
+		c.execute("drop table datas")
 	except sqlite3.OperationalError,x:pass
-	c.execute("create table codes(cid  int,lid  int,code text)")
-	c.execute("create table data(data  BLOB)")
-	db.commit()
+	c.execute("create table codes(cid int primary key,lid int,pid int,uid int,code text)")
+	c.execute("create table datas(pid int primary key,data BLOB)")
 	fs=open(fsql,'r')
 	ifs=fs.__iter__()
 	try:
@@ -29,17 +44,25 @@ def main(fsql,fdata,opt):
 			stmt=getnxtsql(ifs)
 			c.execute(stmt)
 	except StopIteration:pass
-	db.commit()
 	fs.close()
-	fd=open(fdata,'rb')
-	c.execute("insert into data values (?)",(sqlite3.Binary(fd.read()),))
-	fd.close()
+	for pid,fdname in fdata:
+		fd=open(fdname,'rb')
+		c.execute("insert into datas(pid,data) values (?,?)",(pid,sqlite3.Binary(fd.read())))
+		fd.close()
 	db.commit()
 	db.close()
 
+class ArgvException(BaseException):
+	def __init__(self):
+		pass
 if __name__=="__main__":
-	if(len(sys.argv)==4):
-		main(sys.argv[1],sys.argv[2],sys.argv[3])
-	else:
-		print("Usage: %s <sql input> <data input> <opt>"%sys.argv[0])
+	try:
+		if(len(sys.argv)<5 or len(sys.argv)%2==0):
+			raise ArgvException
+		ld=[]
+		for i in xrange(2,len(sys.argv)-1,2):
+			ld.append((int(sys.argv[i]),sys.argv[i+1]))
+		main(sys.argv[1],ld,sys.argv[-1])
+	except ArgvException,x:
+		print("Usage: %s <sql input filename> <data 1 pid> <data 1 filename> ... <data n pid> <data n filename> <output sqlite db>"%sys.argv[0])
 	
